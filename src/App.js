7df91df873e4fb842af19f03f06aece6adde0e93
cernyfr1 <fa.cerny@gmail.com>
2023-11-28 13:14:55 +0100

    implement useFetch for loading items form db and check/uncheck item

--- src/App.js
+++ src/App.js
@@ -3,8 +3,38 @@ import "./App.css";
 import { ChakraProvider, List, VStack } from "@chakra-ui/react";
 import ShoppingItem from "./components/ShoppingItem";
 import AddItem from "./components/AddItem";
+import { useFetch } from "use-http";
+import { useEffect, useState } from "react";
+
+export default App;
 
 function App() {
+  const [shoppingList, setShoppingList] = useState([]);
+  const { get, post, response, loading, error } = useFetch(
+    "http://localhost:9000/shoppingItem",
+  );
+
+  useEffect(() => {
+    loadShoppingList();
+  }, []);
+
+  async function loadShoppingList() {
+    const shoppingList = await get("/list");
+    if (response.ok) setShoppingList(shoppingList);
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h2>{error.message}</h2>
+      </div>
+    );
+  }
+
+  if (loading) {
+    return <h2>loading...</h2>;
+  }
+
   return (
     <ChakraProvider>
       <VStack>
@@ -12,11 +42,16 @@ function App() {
         <AddItem />
         <h2>To buy:</h2>
         <List>
-          <ShoppingItem></ShoppingItem>
+          {shoppingList.map((item) => (
+            <ShoppingItem
+              key={item.id}
+              name={item.content}
+              count={item.count}
+              checked={item.state}
+            />
+          ))}
         </List>
       </VStack>
     </ChakraProvider>
   );
 }
-
-export default App;
--- src/components/ShoppingItem.js
+++ src/components/ShoppingItem.js
@@ -1,20 +1,48 @@
-import { ListItem, Checkbox, IconButton, Flex } from "@chakra-ui/react";
-import { LiaSaveSolid } from "react-icons/lia";
+import { ListItem, Checkbox, IconButton, Flex, Center } from "@chakra-ui/react";
+//import { LiaSaveSolid } from "react-icons/lia";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import {useState} from "react";
+import { useFetch } from "use-http";
 
-function ShoppingItem() {
+function ShoppingItem(props) {
+
+  const {put} = useFetch("http://localhost:9000/shoppingItem");
+  const [isChecked, setIsChecked] = useState(props.checked === "CHECKED");
+
+  async function modifyItem() {
+    await put("/update", {
+      id: "6564d33b789005500bc7809c",
+      content: "Apple",
+      count: 3,
+      state: !isChecked ? "CHECKED" : "UNCHECKED"
+    });
+
+  }
+
+  function handleChange() {
+    modifyItem();
+    setIsChecked(!isChecked);
+  }
+
   return (
     <ListItem
       height={"2.6rem"}
       border={"1px solid"}
       borderColor={"inherit"}
       borderRadius={"0.375rem"}
+      margin={"0.6rem"}
     >
-      <Flex>
-        <Checkbox flex={"1"} size={"lg"}>
-          Item 1
+      <Flex minWidth={"200px"}>
+        <Checkbox
+          flex={"1"}
+          size={"lg"}
+          isChecked={isChecked}
+          onChange={handleChange}
+        >
+          {props.name}
         </Checkbox>
+        <Center marginLeft={"0.6rem"}>{props.count} </Center>
         <IconButton
           aria-label={"Edit button"}
           size={"md"}
